7615e4124238f5f5f4395d1f0f337f52beb8802e
bacari <ok@gmail.com>
2019-05-14 10:19:33 +0200

    Navbar

--- public/index.php
+++ public/index.php
@@ -1,13 +1,6 @@
-<!doctype html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
+<?php
+include ('index2.php')
+?>
 
 <?php
 
@@ -70,6 +63,10 @@ echo "</ul>";
         }
     echo "</ul>";
 ?>
+
+
+<script src="js/jquery-3.4.1.min.js"></script>
+<script src="js/bootstrap.bundle.min.js"></script>
 
 </body>
 </html>
