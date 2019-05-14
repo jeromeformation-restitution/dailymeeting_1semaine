68f3440cabd201201fc488ecc385fa73be5fdee3
bacari <ok@gmail.com>
2019-05-14 09:25:26 +0200

    premier commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+
+$toto = "Salut"; // string
+$tata = 10; // number
+$titi = false; // boolean
+
+echo $toto;
+echo $tata;
+echo "<br>";
+echo (int)$titi;
+
+?>
+
+</body>
+</html>
