dcb31b9a42ee7d86a06ff691432e2df0c9dcc15b
bacari <ok@gmail.com>
2019-05-15 09:31:57 +0200

    constantes / footer / header

---
+++ public/constantes.php
@@ -0,0 +1,13 @@
+<?php
+require ('inc/header.php')
+?>
+<?php
+define("IBRA","bonjour !");
+echo IBRA;
+echo "<br>";
+const IBRA2 = "hello !";
+echo IBRA2;
+?>
+<?php
+require ('inc/footer.php')
+?>
